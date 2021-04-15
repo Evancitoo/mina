fc0b0fafde49c8771dffc30f64ccfa9eefd114c0
Paul Steckler <steck@stecksoft.com>
2021-04-14 17:09:52 -0700

    temp

--- src/lib/bin_prot_layouts/bin_prot_layouts.ml
+++ src/lib/bin_prot_layouts/bin_prot_layouts.ml
@@ -68,3 +68,17 @@ let core_kernel_bigstring_v1 =
   ; type_decl= "Core_kernel.Bigstring.Stable.V1.t"
   ; bin_io_derived= true
   ; bin_prot_rule= Ppx_version_runtime.Bin_prot_rule.Bigstring }
+
+(* TODO: what are the key, value rules? *)
+let snark_work_statement_v1_table =
+  { Ppx_version_runtime.Bin_prot_layout.layout_loc= "None"
+  ; version_opt= None
+  ; type_decl=
+      "(Ledger_proof.Stable.V1.t One_or_two.Stable.V1.t \
+       Priced_proof.Stable.V1.t  * [ `Rebroadcastable of \
+       Core.Time.Stable.With_utc_sexp.V2.t | `Not_rebroadcastable ] ) \
+       Transaction_snark_work.Statement.Stable.V1.Table.t"
+  ; bin_io_derived= true
+  ; bin_prot_rule=
+      Ppx_version_runtime.Bin_prot_rule.(
+        Hashtable {key_rule= String; value_rule= String}) }
